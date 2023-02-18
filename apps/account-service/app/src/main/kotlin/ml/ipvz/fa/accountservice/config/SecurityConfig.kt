0ba530af1303f4ce44f0103f41d2c7b7f80da816
carlosray <carlos.ray@mail.ru>
2023-02-18 08:25:49 +0300

    security configuration

---
+++ apps/account-service/app/src/main/kotlin/ml/ipvz/fa/accountservice/config/SecurityConfig.kt
@@ -0,0 +1,33 @@
+package ml.ipvz.fa.accountservice.config
+
+import ml.ipvz.fa.authservice.base.config.BaseSecurityConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.context.ServerSecurityContextRepository
+
+@Configuration
+@Import(BaseSecurityConfig::class)
+class SecurityConfig(
+    private val serverSecurityContextRepository: ServerSecurityContextRepository
+) {
+
+    @Bean
+    fun filterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http
+            .httpBasic().disable()
+            .formLogin().disable()
+            .csrf().disable()
+            .logout().disable()
+
+            .securityContextRepository(serverSecurityContextRepository)
+
+            .authorizeExchange().anyExchange().authenticated()
+            .and()
+            .build()
+    }
+}
